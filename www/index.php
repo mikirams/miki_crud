4e7e5a2f8f8fe82ae03df6bfe55530c0ab09e648
mikirams <132619513+mikirams@users.noreply.github.com>
2023-10-22 16:57:32 +0200

    zzz

--- www/index.php
+++ www/index.php
@@ -1,11 +1,52 @@
 <?php 
-
 include "header.php"
-
 ?>
 
+<div class="container my-5">
+    <div class="row">
+        <div class="col text-center">
+
+            <div class="card">
+                <div class="card-header display-6">
+                    Acciones sobre la base de datos
+                </div>
+
+                <div class="p-4">
+                <div class="table-responsive">
+                    <table class="table table-primary">
+                        <thead>
+                            <tr>
+                                <th scope="col">Column 1</th>
+                                <th scope="col">Column 2</th>
+                                <th scope="col">Column 3</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr class="">
+                                <td scope="row">R1C1</td>
+                                <td>R1C2</td>
+                                <td>R1C3</td>
+                            </tr>
+                            <tr class="">
+                                <td scope="row">Item</td>
+                                <td>Item</td>
+                                <td>Item</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+                
+
+                </div>
+            </div>
+
+        </div>  
+    </div>
+</div>
+
+
+
+
 <?php 
-
 include "footer.php"
-
 ?>
